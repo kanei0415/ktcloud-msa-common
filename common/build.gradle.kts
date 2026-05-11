b19d20bab142518e3599f4233d27d561da04ffb1
K4NEI <kimyoungho0415@gmail.com>
2026-05-11 14:26:51 +0900

    Split modules [common] to ktcloud-msa-common

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
 
     implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
+    kapt("jakarta.persistence:jakarta.persistence-api")
+    kapt("jakarta.annotation:jakarta.annotation-api")
 }
 
 sourceSets {
